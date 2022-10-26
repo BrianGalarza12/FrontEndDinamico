ca93fd05216a3c88a05751738f34be5e9490b1f8
BrianGalarza <briangalarza71@gmail.com>
2022-10-26 00:02:11 -0300

    añadido componentes para controlar educacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,9 @@ import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +36,8 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     HomeComponent,
     NewExperienciaComponent,
     EditExperienciaComponent,
-    
+    NeweducacionComponent,
+    EditeducacionComponent,    
   ],
   imports: [
     BrowserModule,
